9f4139c7b1dd30879864265b20df9369de14fda2
C1311637 Joao Vitor da Cruz Nascimento Nunes <c1311637@interno.bb.com.br>
2020-12-23 01:18:09 -0300

    Twitter clone completo

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
+
+import { 
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+ } from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <Body>
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <FollowSuggestion 
+              name="Luiz Batanero"
+              nickname="@luizbatanero"
+            />,
+            <FollowSuggestion 
+              name="Luke Morales"
+              nickname="@lukemorales"
+            />,
+            <FollowSuggestion 
+              name="Camila Magalghães"
+              nickname="@camilaamgl"
+            />,
+          ]}
+        />
+        <List 
+          title="Talvez você curta"
+          elements={[
+            <News />,
+            <News />,
+            <News />,
+          ]}
+        />
+      </Body>
+    </Container>
+  );
+}
+
+export default SideBar;
